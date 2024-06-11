479fc705235c563f1b913d3e6d164e62a073a00a
Simon Ball <simonball245@gmail.com>
2024-06-11 14:51:31 +0200

    Add react router and an about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About this Project</h1>
+      <p>This is a React app to leave feedback for a product or service</p>
+      <p>
+        <a href="/">Back to home</a>
+      </p>
+    </Card>
+  );
+}
+
+export default AboutPage;
