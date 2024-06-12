271a54f26a54bf4abf35cda9f3dd2a4a436b7595
Simon Ball <simonball245@gmail.com>
2024-06-12 15:46:17 +0200

    Add delete and update for the json db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -18,8 +18,12 @@ export const FeedbackProvider = ({ children }) => {
     setIsLoading(false);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, {
+        method: 'DELETE',
+      });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -38,11 +42,19 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback]);
   };
 
-  const updateFeedback = (id, updateItem) => {
+  const updateFeedback = async (id, updateItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updateItem } : item,
-      ),
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item)),
     );
 
     setFeedbackEdit({ item: {}, edit: false });
